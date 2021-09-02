07d8e39d2e96af41d229493003e052aeedb77f13
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2021-09-02 20:39:01 +0600

    new last

--- js/main.js
+++ js/main.js
@@ -7,5 +7,27 @@ const searchBook = () => {
     // console.log(url);
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data.docs));
+    .then(data => displaySearchResult(data.docs));
+}
+
+const displaySearchResult = docs => {
+    // console.log(docs);
+    const searchResult = document.getElementById('search-result');
+    docs.forEach(docs => {
+        console.log(docs);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+            <div class="card h-100">
+                <img src="https://covers.openlibrary.org/b/id/554106-M.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Title: ${docs.title.slice(0, 20)}</h5>
+                    <p class="card-text">Author: ${docs.author_name}</p>
+                    <p class="card-text">Publisher: ${docs.publisher}</p>
+                    <p class="card-text">First published: ${docs.first_publish_year}</p>
+                </div>
+            </div>
+        `
+        searchResult.appendChild(div);
+    })
 }
