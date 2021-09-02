502b49b73708a9edb04561b37d55fadeab9a86a5
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2021-09-02 21:46:53 +0600

    another

--- js/main.js
+++ js/main.js
@@ -1,27 +1,24 @@
 const searchBook = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    // console.log(searchText);
     searchField.value = '';
     const url = `https://openlibrary.org/search.json?q=${searchText}`;
-    // console.log(url);
     fetch(url)
     .then(res => res.json())
     .then(data => displaySearchResult(data.docs));
 }
 
 const displaySearchResult = docs => {
-    // console.log(docs);
     const searchResult = document.getElementById('search-result');
-    docs.forEach(docs => {
+    docs.slice(0, 20).forEach(docs => {
         console.log(docs);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
             <div class="card h-100">
-                <img src="https://covers.openlibrary.org/b/id/554106-M.jpg" class="card-img-top" alt="...">
+                <img src="https://covers.openlibrary.org/b/id/${docs.cover_i}-M.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">Title: ${docs.title.slice(0, 20)}</h5>
+                    <h5 class="card-title">Title: ${docs.title}</h5>
                     <p class="card-text"><span class="card-text-bold">Author:</span> ${docs.author_name}</p>
                     <p class="card-text"><span class="card-text-bold">Publisher:</span> ${docs.publisher}</p>
                     <p class="card-text"><span class="card-text-bold">First Published:</span> ${docs.first_publish_year}</p>
